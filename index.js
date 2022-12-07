2f20766dc4f1380e17beb5e292c0136f6f8d8a96
amorimlt <julianeamorimlt@gmail.com>
2022-12-06 22:37:20 -0300

    finaliza style

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 const handleSearch = async (event) => {
   event.preventDefault();
 
-  // Exibir mensagem de carregamento
   const message = document.querySelector('#message');
   message.innerHTML = 'buscando...';
 
-  // Limpa lista de programas
   const listaDeProgramas = document.querySelector('#shows');
   listaDeProgramas.innerHTML = '';
-
-  // Obter o texto digitado pelo usuário
 
   const caixaDeBusca = document.querySelector('#query');
   const textoASerBuscado = caixaDeBusca.value;
 
-  // Formar a URL de consulta
   const url = `https://api.tvmaze.com/search/shows?q=${textoASerBuscado}`;
 
-  // Realizar a consulta na API de forma assíncrona
   const resposta = await fetch(url);
   const programas = await resposta.json();
 
-  // Finaliza o procedimento caso não haja nenhum resultado
   if (programas.length === 0) {
-    // Exibir mensagem de não encontrado
     message.innerHTML = 'nenhum resultado encontrado.';
     return;
   }
 
-  // Limpar a mensagem
   message.innerHTML = '';
 
-  // Iterar pelos programas
   programas.forEach((programa) => {
-    // Obter os dados do programa
     const titulo = programa?.show?.name || '';
     const imagem = programa?.show?.image?.medium || '';
 
-    // Inserir os programas na lista de resultados
     listaDeProgramas.insertAdjacentHTML(
       'beforeend',
       `
